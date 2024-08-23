06340ca803c899b334acec6d27d590b524769b9f
gabriel-garciae <eng.gabrielgevangelista@gmail.com>
2024-08-23 18:49:37 -0300

    Feat: criação da dag

--- dags/dag_extracao.py
+++ dags/dag_extracao.py
@@ -3,14 +3,16 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import os
 
-# Função para executar o código de extração
-#def run_extracao():
-#    exec(open("/opt/duckdb/raw/extracao.py").read())
-
+#função para executar o código de extração
 def run_extracao():
-    print("Hello Airflowwwwwwwwwwwwww")
+   exec(open("/opt/duckdb/raw/extracao.py").read())
 
-# Definição da DAG
+#função para executar o código de extração
+def run_carregamento():
+   exec(open("/opt/motherduck/input_dw.py").read())
+
+
+#definição da DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -31,4 +33,9 @@ with DAG(
         python_callable=run_extracao,
     )
 
-    task_extracao
+    task_carregamento = PythonOperator(
+        task_id='run_carregamento',
+        python_callable=run_carregamento,
+    )
+
+    task_extracao >> task_carregamento
